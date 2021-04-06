55aaec34aca5d4ab48b9011a3ac2f2ea57d4db0f
Sergey Butyanov <igor@tomatstudio.ru>
2021-04-06 13:36:35 +0300

    AnswersList component, error handler

---
+++ src/components/AnswerList.vue
@@ -0,0 +1,59 @@
+<template>
+    <div>
+        <p><small>{{answersArray}}</small></p>
+        <div class="two-columns">
+            <div class="col">
+                <p>Правильные ответы:</p>
+                <ul class="true-list">
+                    <li v-for="el in trueList" :key="el">{{el}}</li>
+                </ul>
+            </div>
+            <div class="col">
+                <p>Осталось не отгаданных слов:</p>
+                <p class="enemy-dmg">{{answersArray.length}}</p>
+            </div>
+        </div>
+        
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        answersArray: Array,
+        trueList: Array
+    }
+}
+</script>
+
+<style scoped>
+    .two-columns {
+        margin: 0 auto;
+        max-width: 450px;        
+        display: flex;
+        align-items: flex-start;
+    }
+
+    .col {
+        padding: 20px;
+        min-height: 150px;
+        width: 50%;
+        border: 1px dotted #ccc;
+    }
+
+    .enemy-dmg {
+        color: red;
+        font-weight: bold;
+    }
+
+    p {
+        margin: 0;
+    }
+
+    .true-list {   
+        margin: 0;     
+        padding-left: 0;        
+        list-style: none;
+        color: green;
+    }
+</style>
--- src/components/UserInput.vue
+++ src/components/UserInput.vue
@@ -1,19 +1,19 @@
 <template>
-    <form>
-        <input type="text" v-model="current">
-        <button @click.prevent="check">Check</button>
-        <p v-if="message">{{message}}</p>
-
-        {{answersArray}}
-
-        <p v-for="el in trueList" :key="el">{{el}}</p>
-    </form>
-    
+    <div>
+        <form>
+            <input type="text" v-model="current" :class="{'input-error' : alertMessage}">
+            <button @click.prevent="check">Check</button>
+            <p v-if="alertMessage" style="color: red;">{{alertMessage}}</p>                
+        </form>
+        <answer-list :answersArray="answersArray" :trueList="trueList"/>        
+    </div>
 </template>
 
 <script>
+import AnswerList from './AnswerList.vue';
 
 export default {
+    components: { AnswerList },
     props: {
         answers: Array
     },    
@@ -22,34 +22,31 @@ export default {
         return {
             current: null,
             answersArray: [...this.answers],
-            message: null
-        }
-    },
-
-    computed: {
-        trueList() {
-            return [
-
-            ]
+            alertMessage: null,
+            trueList: []
         }
     },
 
     methods: {
         check() {
-            if(!this.current) {
-                this.message = 'Вы ничего не ввели'
+            if(!this.current) {                
+                this.alertMessage = 'Вы ничего не ввели';
                 return
             }
-
+            this.alertMessage = null;
+            
             let searchWord = this.current.toLowerCase();
-
-            if(this.answersArray.includes(searchWord)) {                
+            if(this.answersArray.includes(searchWord)) {
                 this.answersArray.splice(this.answersArray.indexOf(searchWord), 1);
 
-                this.trueList.push(searchWord);
-                this.current = null;
-            }           
-        }
+                this.trueList.push(searchWord);                
+            } else {
+                this.alertMessage = 'Слово не подходит';
+            }
+
+            this.current = null;
+        },       
+        
     }
 }
 </script>
@@ -62,7 +59,13 @@ export default {
      margin-top: 20px;
      margin: 7px;
      font-size: 20px;
+     border: 1px solid;
      text-transform: uppercase;
      text-align: center;
+     outline: none;
+ }
+
+ .input-error {
+     border-color: red;
  }
 </style>
